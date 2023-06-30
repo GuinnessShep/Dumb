afbbca9951a408d6ce4dec04d01cf851f63c0894
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2023-06-29 20:58:04 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,12 +4,21 @@ cd /app/install
 wget https://github.com/GuinnessShep/Dumb/raw/main/tunnelto
 chmod +x tunnelto
 tunnelto set-auth --key LPiLOhptMIl4Kbn6zgMw1u
-wget https://github.com/oobabooga/one-click-installers/raw/main/start_linux.sh
-wget https://github.com/oobabooga/one-click-installers/raw/main/update_linux.sh
-wget https://github.com/GuinnessShep/selfrun/raw/main/webui.py
-bash start_linux.sh
-ls
-pwd
+git clone -b v1.0 https://github.com/camenduru/text-generation-webui
+cd text-generation-webui
+pip install -r requirements.txt
+pip install -U gradio==3.28.3
+mkdir /content/text-generation-webui/repositories
+cd /content/text-generation-webui/repositories
+git clone -b v1.0 https://github.com/camenduru/GPTQ-for-LLaMa.git
+cd GPTQ-for-LLaMa
+python3 setup_cuda.py install
+cd ..
+git clone https://github.com/PanQiWei/AutoGPTQ.git
+cd AutoGPTQ
+pip install .
+cd ..
+cd ..
 python3 download-model.py TheBloke/Tulu-30B-SuperHOT-8K-GPTQ --branch main
 python3 download-model.py TheBloke/WizardLM-33B-V1.0-Uncensored-GPTQ --branch main
 python3 download-model.py TheBloke/airoboros-65B-gpt4-1.4-GGML --branch main
