1d43d8c9febb9f56f3cea8fcaf5028687571030c
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2023-06-29 18:49:11 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-
-# Set the authentication key
-tunnelto set-auth --key GeBoAW1CSmWK5SbfOXUmU8
-
-# Run the tunnelto command with the desired parameters
-tunnelto --subdomain ilovedogshit --port 7860 &
-
-# Run the Python script in the background
-python3 /app/server.py --chat --share --model LLaMA --auto-devices --extensions gallery send_pictures character_bias &
-
-# Wait for both processes to finish
+wget https://github.com/GuinnessShep/Dumb/raw/main/tunnelto
+chmod +x tunnelto
+tunnelto set-auth --key LPiLOhptMIl4Kbn6zgMw1u
+tunnelto --subdomain ilovedogshit --port 7860 & python3 /app/server.py --chat --share --model LLaMA --auto-devices --extensions gallery send_pictures character_bias
 wait
