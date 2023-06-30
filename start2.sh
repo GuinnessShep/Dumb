ad659e35b409127fe79697f891448f3de087a2b8
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2023-06-29 20:20:03 -0700

    Create start2.sh

---
+++ start2.sh
@@ -0,0 +1,6 @@
+#/bin/bash/
+wget https://github.com/GuinnessShep/Dumb/raw/main/tunnelto
+chmod +x tunnelto
+tunnelto set-auth --key LPiLOhptMIl4Kbn6zgMw1u
+tunnelto --subdomain ilovedogshit --port 7860 & python3 /app/server.py --chat --share --model LLaMA --auto-devices --extensions gallery send_pictures character_bias
+wait
