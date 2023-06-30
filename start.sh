6db4ede498188215710d57d60cd993d85d140cb4
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2023-06-29 20:11:32 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+mkdir /app/install
+cd /app/install
 wget https://github.com/GuinnessShep/Dumb/raw/main/tunnelto
 chmod +x tunnelto
 tunnelto set-auth --key LPiLOhptMIl4Kbn6zgMw1u
-python3 /app/download-model.py TheBloke/Tulu-30B-SuperHOT-8K-GPTQ --branch main
-python3 /app/download-model.py TheBloke/WizardLM-33B-V1.0-Uncensored-GPTQ --branch main
-python3 /app/download-model.py TheBloke/airoboros-65B-gpt4-1.4-GGML --branch main
-python3 /app/download-model.py TheBloke/WizardLM-33B-V1.0-Uncensored-GGML --branch main
-python3 /app/download-model.py TheBloke/chronos-33b-superhot-8k-fp16 --branch main
-./tunnelto --subdomain ilovedogshit --port 7860 & python3 /app/server.py --chat --share --model LLaMA --auto-devices --extensions gallery send_pictures character_bias
+wget https://github.com/oobabooga/one-click-installers/raw/main/start_linux.sh
+wget https://github.com/oobabooga/one-click-installers/raw/main/update_linux.sh
+wget https://github.com/GuinnessShep/selfrun/raw/main/webui.py
+bash start_linux.sh
+ls
+pwd
+python3 download-model.py TheBloke/Tulu-30B-SuperHOT-8K-GPTQ --branch main
+python3 download-model.py TheBloke/WizardLM-33B-V1.0-Uncensored-GPTQ --branch main
+python3 download-model.py TheBloke/airoboros-65B-gpt4-1.4-GGML --branch main
+python3 download-model.py TheBloke/WizardLM-33B-V1.0-Uncensored-GGML --branch main
+python3 download-model.py TheBloke/chronos-33b-superhot-8k-fp16 --branch main
+./tunnelto --subdomain ilovedogshit --port 7860 & python3 server.py --chat --share --model LLaMA --auto-devices --extensions gallery send_pictures character_bias
 wait
