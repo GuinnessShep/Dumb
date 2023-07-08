7ae3f6b60d4342042b581d0fbeb32bbdb13d517c
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2023-07-08 14:48:00 -0700

    Update start2.sh

--- start2.sh
+++ start2.sh
@@ -1,6 +1,6 @@
 #/bin/bash/
-wget https://github.com/GuinnessShep/Dumb/raw/main/tunnelto
-chmod +x tunnelto
-tunnelto set-auth --key LPiLOhptMIl4Kbn6zgMw1u
-tunnelto --subdomain ilovedogshit --port 7860 & python3 /app/server.py --chat --share --model LLaMA --auto-devices --extensions gallery send_pictures character_bias
+wget https://github.com/agrinman/tunnelto/releases/download/0.1.18/tunnelto-linux.tar.gz
+tar -xvzf tunnelto-linux.tar.gz
+./tunnelto set-auth --key LPiLOhptMIl4Kbn6zgMw1u
+./tunnelto --subdomain ilovedogshit --port 7860 & python3 /app/server.py --chat --share --model LLaMA --auto-devices --extensions gallery send_pictures character_bias
 wait
