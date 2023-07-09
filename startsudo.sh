1b317ae931d37170277487f9ab94b63bd1b09a6d
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2023-07-08 23:25:55 -0700

    Update startsudo.sh

--- startsudo.sh
+++ startsudo.sh
@@ -78,6 +78,4 @@ cd ..
 cd ..
 pip install markdown
 cd /working/text-generation-webui
-curl -o runtmate.sh https://raw.githubusercontent.com/GuinnessShep/Dumb/main/tmate.sh
-chmod +x tmate.sh
-tunnelto --port 7860 & python3 server.py --chat --share --model LLaMA --auto-devices --extensions gallery send_pictures character_bias
+./tunnelto --port 7860 & python3 server.py --chat --share --model LLaMA --auto-devices --extensions gallery send_pictures character_bias
